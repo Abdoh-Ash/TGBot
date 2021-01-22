57f1b1b83a763668897d4bf30ec12c0e53a25a9a
Abdulrahman Alkhateeb <54361220+Abdoh-Ash@users.noreply.github.com>
2021-01-22 22:52:48 +0200

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,31 @@
+TOKEN = "1598166798:AAHB53uMiDjh0LQldlw0m--CsQw6BGWAw-E"
+
+WELCOME = "Hello, {0}!\n\nI'm Adam, a funny and friendly chatbot.\n\n“Software is like entropy: It is difficult to grasp, weighs nothing, and obeys the Second Law of Thermodynamics; i.e., it always increases.”\n— Norman Augustine"
+HELP = """You can control me by using these commands:
+
+General:\nStart me: /start\nAbout me: /about\nPing me: /ping\nMy purpose: /purpose\nCommands list: /help\n
+Features:\nWikipedia Quick Search: /wikipedia\n
+"""
+
+ABOUT = "I'm a chatbot, by definition.\n\nA chatbot is a software application used to conduct an on-line chat conversation via text or text-to-speech, in lieu of providing direct contact with a live human agent.\n\nDesigned to convincingly simulate the way a human would behave as a conversational partner, chatbot systems typically require continuous tuning and testing, and many in production remain unable to adequately converse or pass the industry standard Turing test.\n\nThe term \"ChatterBot\" was originally coined by Michael Mauldin (creator of the first Verbot) in 1994 to describe these conversational programs."
+PURPOSE = "I exist for multiple reasons, but in general, we could say that I'm more of an experiment for fun. However, I believe that I have a great potential for success."
+PING = "Don't worry, still alive and kicking!"
+SORRY = "I hate to say this, but unfortunately, I'm not sophisticated enough to understand and react to what you're saying."
+
+
+WIKIPEDIA = "Wikipedia is a multilingual open-collaborative online encyclopedia created and maintained by a community of volunteer editors using a wiki-based editing system. It is one of the 15 most popular websites as ranked by Alexa, as of January 2021 and The Economist newspaper placed it as the \"13th-most-visited place on the web\". Featuring no advertisements, it is hosted by the Wikimedia Foundation, an American non-profit organization funded primarily through donations.\n\nSend me a keyword so that I can get you a summary from Wikipedia using it:"
+
+ABOUT_TC = "Thought Café is a virtual community of brilliant-minded individuals, with the objective of sharing knowledge, insight, and wisdom.\n\nWe discuss countless topics on regular basis, we bring up questions, share opinions and search for answers.\nThere is no end to education. It is not that you read a book, pass an examination, and finish with education. The whole of life, from the moment you are born to the moment you die, is a process of learning. Learning is a lifelong process.\n\nHumans are social creatures by nature, so don't worry, you'll enjoy the ride with us!"
+GUIDELINES_TC = """The rules and guidelines for our community are as follows:
+
+1. Be Kind and Courteous
+We're all in this together to create a welcoming environment. Let's treat everyone with respect. Healthy debates are natural, but kindness is required.
+
+2. No Promotions or Spam
+Give more than you take to this group. Self-promotion, spam and irrelevant links aren't allowed.
+
+3. Respect Other Views
+“I am convinced about the veracity of my opinions, but I do consider it likely that they may turn out to be incorrect. Likewise, I am convinced about the incorrectness of the views different from mine, but I do concede the possibility that they may turn out to be correct.” — Imam Shafa’i
+
+And finally, feel at home!
+Don't be an observer all the time, get engaged and be active."""
